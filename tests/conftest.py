4de2f614fd1233ebdce89ffcba4f7e6708e1957f
jgd10 <jamesgordonderrick@gmail.com>
2022-01-28 11:06:48 +0000

    Added tests for all functions in generator
    Simple tests added for all funtctions in generator, however some
    refactoring accompanied them which is in the next commit.

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from common import create_text_file_in_directory
+
+
+@pytest.fixture()
+def destination_directory(tmp_path_factory):
+    destination_path = tmp_path_factory.mktemp('destination')
+    return destination_path
+
+
+@pytest.fixture()
+def dummy_templates_path(tmp_path):
+    templates_path = tmp_path / 'templates'
+    templates_path.mkdir(parents=True)
+    shared_path = templates_path / 'shared'
+    shared_path.mkdir(parents=True)
+
+    for name in ['README.md', 'README.rst', 'README.txt', '.dummy']:
+        create_text_file_in_directory(templates_path, name)
+    return templates_path
+
+

3743c3a2075c0eef53407f3e766cdcadbaadc616
jgd10 <jamesgordonderrick@gmail.com>
2022-02-24 11:07:41 +0000

    added test for new replacement: '-' is replaced with '_' in setup.py for the proj name

--- tests/test_main.py
+++ tests/test_main.py
@@ -62,10 +62,10 @@ class TestGetTemplatesPath:
 
 class TestApplyTemplate:
     @pytest.mark.parametrize('proj_name', ['new', 'new2', '2new', '22',
-                                           'new-2', 'new two', ' new '])
+                                           'new two', ' new '])
     def test_project_name_rename(self, proj_name, tmp_path):
         s = '$project_name'
-        test_files = [tmp_path / f'test.{suffix}'
+        test_files = [tmp_path / f'test{suffix}'
                       for suffix in ['.py', '.txt', '.md']]
         for test_file in test_files:
             with open(test_file, 'w') as f:
@@ -75,3 +75,21 @@ class TestApplyTemplate:
             with open(test_file, 'r') as f:
                 s = f.read()
             assert proj_name in s
+
+    def test_project_name_rename_removes_hyphens_in_setup_dot_py(self,
+                                                                 tmp_path):
+        proj_name = 'new-2'
+        s = '$project_name'
+        test_files = [tmp_path / f'setup{suffix}'
+                      for suffix in ['.py', '.txt', '.md']]
+        for test_file in test_files:
+            with open(test_file, 'w') as f:
+                f.write(s)
+        add_project_name_to_files(test_files, proj_name)
+        for test_file in test_files:
+            with open(test_file, 'r') as f:
+                s = f.read()
+            if test_file.suffix == '.py':
+                assert proj_name.replace('-', '_') in s
+            else:
+                assert proj_name in s

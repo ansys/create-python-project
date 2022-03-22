ee267cf811a65a93cf5c523916550e69177ff695
Babacar Fall <babacar.fall@ansys.com>
2022-03-22 15:14:28 +0100

    add observability first pieces

--- src/templates/gRPC-api/src/server.py
+++ src/templates/gRPC-api/src/server.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2022, Ansys Inc. Unauthorised use, distribution or duplication is prohibited
-
 from concurrent import futures
 import logging
 import grpc
@@ -10,16 +8,19 @@ from pathlib import Path
 
 import stubs.pingserver_pb2_grpc
 from services.pinger import Pinger
+from observability.logging import Logging
 
 
 def serve():
+    logger = Logging().logger
+    port = 50051
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     stubs.pingserver_pb2_grpc.add_PingerServicer_to_server(Pinger(), server)
-    server.add_insecure_port('[::]:50051')
+    logger.info(f'Server starting and listening on {port}')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
